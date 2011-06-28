e63dd3d32fff38d96a085076a6f2529b19fa7595
Patricio Palma <mail@ppalma.cl>
2011-06-28 11:02:40 -0400

    Ignoring last gmt hour,(incomplete file)

--- gcf2sac.py
+++ gcf2sac.py
@@ -1,8 +1,14 @@
 #! /usr/bin/python
 
 import pyinotify
-import os
+import os,string
 import ConfigParser
+import datetime
+import signal
+import sys
+from datetime import date
+from ftplib import FTP
+from time import gmtime, strftime
 
 class Identity(pyinotify.ProcessEvent):
     def process_default(self, event):
@@ -31,15 +37,56 @@ class Identity(pyinotify.ProcessEvent):
 def on_loop(notifier):
     s_inst = notifier.proc_fun().nested_pevent()
 
+def signal_handler(signal, frame):
+	notifier.stop()
+	sys.exit(0)
+
 
 config = ConfigParser.ConfigParser()
 config.read('/opt/gcf2sac/gcf2sac.cfg')
+print 'Loaded config file'
+signal.signal(signal.SIGINT, signal_handler)
+
 wm = pyinotify.WatchManager()
 s = pyinotify.Stats()
-notifier = pyinotify.Notifier(wm, default_proc_fun=Identity(s), read_freq=5)
+notifier = pyinotify.ThreadedNotifier(wm, Identity())
+notifier.start()
 wm.add_watch(config.get('FOLDER','watch'), pyinotify.IN_CLOSE_WRITE, rec=True, auto_add=True)
 print 'watching %s'%config.get('FOLDER','watch')
-notifier.loop(callback=on_loop)
 
+print 'Connecting'
+ftp = FTP(
+        config.get('FTP', 'address'),
+        config.get('FTP', 'user'),
+        config.get('FTP', 'passwd')
+        )
+ftp.cwd(config.get('FTP', 'folder'))
+print 'Loading Remote Tree'
+x=[]
+ftp.dir('-d','*/',lambda L:x.append(L.split()[-1]))
+remoteTree ={}
+for dir in x:
+        ftp.cwd(dir)
+        y = []
+        ftp.dir('-d','*/',lambda L:y.append(L.split()[-1]))
+        remoteTree[dir]=y
+        ftp.cwd('..')
+today = date.today()
 
+for dir in remoteTree:
+        for d in remoteTree[dir]:
+                output = '%s/%s%s'%(config.get('FOLDER','location'),dir,d)
+                match = today.strftime('%Y%m%d')+'*'
+                link = 'ftp://%s:%s@%s%s/%s%s%s'%(
+                        config.get('FTP', 'user'),
+                        config.get('FTP', 'passwd'),
+                        config.get('FTP', 'address'),
+                        '/TramaDatos',#config.get('FTP', 'folder'),
+                        dir,
+                        d,
+                        match
+                        )
+		cmd = 'wget -qnc -P %s %s -R *_%s*'%(output,link,strftime("%H", gmtime()))
+                print cmd
+                os.popen(cmd)
 
--- luncher.sh
+++ luncher.sh
@@ -1,5 +1,5 @@
 xterm -e "python /opt/gcf2sac/gcf2sac-auto.py" &
 sleep 1s  
 xterm -e "python /opt/gcf2sac/gcf2sac-manual.py" &
-sleep 1s 
-xterm -e "watch ls -lR /home/ppalma/Desktop/COY2"
+#sleep 1s 
+#xterm -e "watch ls -lR /home/ppalma/Desktop/COY2"
